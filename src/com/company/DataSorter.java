031a1dc218c1a47aed68c4426c49db18ce629a70
fourthwall <harry@simans.net>
2015-07-15 13:25:29 +0100

    Initial commit

---
+++ src/com/company/DataSorter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by harrison on 15/07/15.
+ */
+public class DataSorter {
+    public static File[] PFA = CreatePFA();
+
+    private static File[] CreatePFA() {
+        //read start and end of pertinent files from log and create an array of them;
+        return new File[0];
+    }
+
+}
